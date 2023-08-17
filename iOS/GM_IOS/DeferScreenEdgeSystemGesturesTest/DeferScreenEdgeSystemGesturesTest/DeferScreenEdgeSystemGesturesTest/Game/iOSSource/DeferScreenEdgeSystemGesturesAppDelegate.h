fbdd1b3da893d112b64e5d34f7b5d2f03d58b1cd
Jock Findlay <jock@haikuinteractive.com>
2023-08-17 14:40:38 +0100

    Build for iOS

---
+++ iOS/GM_IOS/DeferScreenEdgeSystemGesturesTest/DeferScreenEdgeSystemGesturesTest/DeferScreenEdgeSystemGesturesTest/Game/iOSSource/DeferScreenEdgeSystemGesturesAppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  DeferScreenEdgeSystemGestures.h
+//
+
+#ifndef DeferScreenEdgeSystemGesturesAppDelegate_h
+#define DeferScreenEdgeSystemGesturesAppDelegate_h
+
+#import "iPad_RunnerAppDelegate.h"
+
+@interface DeferScreenEdgeSystemGesturesAppDelegate : iPad_RunnerAppDelegate
+
+@end
+
+#endif /* DeferScreenEdgeSystemGesturesAppDelegate_h */
